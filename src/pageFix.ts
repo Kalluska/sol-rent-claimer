f46e672655977c47051d16007ede5c6d4dfaa541
MrAmalone <kalle.etelaaho15@gmail.com>
2026-04-16 09:54:40 +0300

    Remove all old TypeScript files

--- src/pageFix.ts
+++
@@ -1,9 +0,0 @@
-export function handlePageSwitch(setView: any, hiddenWalletBtnRef: any) {
-  setView("app");
-
-  setTimeout(() => {
-    if (hiddenWalletBtnRef?.current) {
-      hiddenWalletBtnRef.current.click();
-    }
-  }, 100);
-}
