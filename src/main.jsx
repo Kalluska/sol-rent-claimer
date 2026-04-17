9cc834408ec4c34b60dab5738b390a9eb08ad165
MrAmalone <kalle.etelaaho15@gmail.com>
2026-04-17 10:36:17 +0300

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,11 @@ import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
 import { clusterApiUrl } from '@solana/web3.js'
 import App from './App'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import '@solana/wallet-adapter-react-ui/styles.css'
 const wallets = [new PhantomWalletAdapter()]
+inject()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ConnectionProvider endpoint={`https://mainnet.helius-rpc.com/?api-key=${import.meta.env.VITE_HELIUS_API_KEY}`}>
